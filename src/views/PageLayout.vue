e512571c0ea71fa559e75cb3209a7a19e7a9af66
hailan <phr9000@gmail.com>
2021-11-27 20:31:31 +0900

    test

--- src/views/PageLayout.vue
+++ src/views/PageLayout.vue
@@ -5,11 +5,11 @@
     <!-- 메인사진  -->
     <div class="main_container">
       <div class="main_in">
-        <img
+        <!-- <img
           src="../../src/assets/main.jpg"
           alt=""
           style="width: 100%; object-fit: cover; height: 100%"
-        />
+        /> -->
         <div>
           <div class="main_txt_area">
             <h2 style="font-size: 3.5rem">Welcome Hero</h2>
@@ -33,27 +33,80 @@
         </div>
       </div>
     </div>
-    <!-- section  -->
-    <div style="padding: 1rem 2rem">
-      <section>
-        <h3 style="color: #fff; font-size: 1.7rem">About Story</h3>
-        <div class="txt story_txt">
-          옛날 옛적, 세계는 사악한 사흉신에 의해 멸망의 위기에 있었다.
-          <br />이때, 세계의 균형을 지키는 사방신은 땅을 수호하는 십이지신을
-          불러 모아, 사흉신과 맞서 싸웠다. 강력한 사흉신의 힘 때문에, 십이지신은
-          본인들의 영혼을 담보로 사흉신을 봉인하는데 성공했다. <br />그 결과,
-          세계 곳곳으로 영혼의 조각이 흩어진 십이지신은 시간이 지남에 따라 역사
-          속에서 사라졌다.<br />
-          약 3000년이 후, 인간들의 커져가는 탐욕과 욕망, 악인들의 횡포로
-          십이지신의 봉인이 약해지게 되었고 사흉신은 점차 본인들의 힘을 되찾고
-          있었다. <br />이를 느낀 사방신은 다시 한 번 십이지신을 불러 모으려고
-          했지만, 12000개의 영혼으로 흩어진 십이지신을 찾기란 불가능 했다.
-          <br />그때, 세계 곳곳에서 십이지신의 영혼의 힘을 얻은 영웅들이
-          등장하게 되는데.. 진정한 십이지신의 모습을 되찾고, 세상을 구할 영웅은
-          누가 될 것 인가?
+
+    <!-- about story ko  -->
+    <div style="padding: 1rem 1.5rem">
+      <section style="color: #fff">
+        <div
+          data-aos="fade-left"
+          class="card"
+          style="
+            max-width: 70%;
+            margin: 10x auto;
+            line-height: 25px;
+            font-size: 20px;
+          "
+        >
+          <h3 class="aboutStory_txt" style="color: #fff; font-size: 1.7rem">
+            About Story
+          </h3>
+          <div style="padding-left: 1.5rem">
+            옛날 옛적, 세계는 사악한 사흉신에 의해 멸망의 위기에 있었다.
+            <br />이때, 세계의 균형을 지키는 사방신은 땅을 수호하는 십이지신을
+            불러 모아, 사흉신과 맞서 싸웠다. 강력한 사흉신의 힘 때문에,
+            십이지신은 본인들의 영혼을 담보로 사흉신을 봉인하는데 성공했다.
+            <br />그 결과, 세계 곳곳으로 영혼의 조각이 흩어진 십이지신은 시간이
+            지남에 따라 역사 속에서 사라졌다.<br />
+            약 3000년이 후, 인간들의 커져가는 탐욕과 욕망, 악인들의 횡포로
+            십이지신의 봉인이 약해지게 되었고 사흉신은 점차 본인들의 힘을 되찾고
+            있었다. <br />이를 느낀 사방신은 다시 한 번 십이지신을 불러 모으려고
+            했지만, 12000개의 영혼으로 흩어진 십이지신을 찾기란 불가능 했다.
+            <br />그때, 세계 곳곳에서 십이지신의 영혼의 힘을 얻은 영웅들이
+            등장하게 되는데.. 진정한 십이지신의 모습을 되찾고, 세상을 구할
+            영웅은 누가 될 것 인가?
+          </div>
         </div>
       </section>
     </div>
+
+    <!-- about story in  -->
+    <section>
+      <div
+        data-aos="fade-left"
+        class="card"
+        style="
+          max-width: 70%;
+          margin: 10px;
+          line-height: 27px;
+          color: #fff;
+          font-size: 22px;
+          text-align: left;
+          padding-left: 2rem;
+        "
+      >
+        Once upon a time, the world was on the verge of extinction by The four
+        evil monsters. At this time, Sabangsin(four bearing god?), who protected
+        the balance of the world, gathered The Twelve Ground Guards who
+        protected the land and fought against The four evil monsters. However
+        The four evil monsters were very powerful. The Twelve Ground Guards
+        succeeded in sealing the evils with their souls as collateral. As a
+        result, The Twelve Ground Guards‘s fragments of soul were scattered all
+        over the world. Over time, it has been forgotten in history. About 3,000
+        years later, due to the growing greed, desire, and tyranny of the
+        wicked, the seal of The Twelve Ground Guards was weakened, and the evils
+        spirit was gradually regaining its power. Feeling this, Sabangsin(four
+        bearing god?) once again tried to gather The Twelve Ground Guards.
+        However, It was impossible to find 12,000 scattered souls. At that time,
+        heroes who gained the power of the spirit of the twelve ground guards
+        appeared from all over the world.. Who will be the hero who will regain
+        the true image of the twelve ground guard and save the world?
+      </div>
+    </section>
+
+    <!-- about story in  -->
+    <section style="padding-left: 1.5rem">
+      <h3 class="nfts_txt" style="color: #fff; font-size: 1.7rem">NFTs</h3>
+    </section>
   </div>
 </template>
 
@@ -79,12 +132,16 @@ header {
 }
 .main_container {
   width: 100%;
-  height: 400px;
+  height: 700px;
+  background-image: url("../../src/assets/main.jpg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center center;
   position: relative;
 }
 
 .main_in {
-  height: 400px;
+  height: 100%;
   color: #fff;
 }
 
@@ -140,8 +197,21 @@ h3 {
   animation: aniload 3s;
 }
 
-h3::after {
+.aboutStory_txt::after {
   content: "About Story";
+  display: flex;
+  transform: rotateX(180deg);
+  background-image: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0) 10%,
+    rgba(255, 255, 255, 0.5)
+  );
+  -webkit-background-clip: text;
+  color: transparent;
+}
+
+.nfts_txt::after {
+  content: "NFTs";
   display: flex;
   transform: rotateX(180deg);
   background-image: linear-gradient(
